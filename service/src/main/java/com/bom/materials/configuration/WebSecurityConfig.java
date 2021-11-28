929b2468fd05e0ef6efdb322559027df50c21afa
Shubham Jain <shubhrjain7@gmail.com>
2021-11-28 12:40:49 +0530

    Add Spring Security

---
+++ service/src/main/java/com/bom/materials/configuration/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.bom.materials.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Disable CORS for request coming from front end
+        http.cors().and()
+                // To Remove 403 forbidden errors
+                .csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
